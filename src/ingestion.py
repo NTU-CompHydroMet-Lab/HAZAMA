246d8e3b4a57dbaea6744269dd26ec9d5d8513b1
chunen <chunen0101@gmail.com>
2026-01-31 17:05:19 +0800

    ingestion_0131_1

--- src/ingestion.py
+++ src/ingestion.py
@@ -4,6 +4,8 @@ import json
 import numpy as np
 from tqdm import tqdm
 import sys
+import geopandas as gpd
+from shapely.geometry import Point
 
 
 # ----------------------------------------------
@@ -19,7 +21,7 @@ def initialize_gee(MY_GEE_PROJECT):
 
 
 # ----------------------------------------------
-# Define a function to safely parse 'Admin Units'
+# Define a function to safely parse 'Admin Units' or 'GADM Admin Units'
 def parse_admin_units_safe(x):
     if pd.isna(x) or x == '':
         return []
@@ -30,8 +32,8 @@ def parse_admin_units_safe(x):
 
 
 # ----------------------------------------------
-# Extract information for adm1 and adm2 from the 'Admin Units' list of a certain event
-def reorganize_admin_data_gaul(admin_list):
+# Extract information for adm1 and adm2 from the 'Admin Units', 'GADM Admin Units' list of a certain event
+def reorganize_admin_data(admin_list, admin_list_gadm=None):
     # Check admin_list is valid list, return [{}] if not
     if not isinstance(admin_list, list) or not admin_list:
         return [{}]
@@ -42,10 +44,23 @@ def reorganize_admin_data_gaul(admin_list):
     
     # Collect all ADM2 entries
     adm2_entries = [x for x in admin_list if x.get('adm2_name') or x.get('adm2_code')]
+
+    # ----------------------------------------------
+    # Check admin_list_gadm is valid list, return [{}] if not
+    if not isinstance(admin_list_gadm, list) or not admin_list_gadm:
+        return [{}]
     
+    # Collect all ADM1 information (using set to remove duplicates, filter out None/Empty)
+    adm1_names_gadm = sorted(list(set([x.get('name_1') for x in admin_list_gadm if x.get('name_1')])))
+    adm1_codes_gadm = sorted(list(set([x.get('gid_1') for x in admin_list_gadm if x.get('gid_1')])))
+    
+    # Collect all ADM2 entries
+    adm2_entries_gadm = [x for x in admin_list_gadm if x.get('name_2') or x.get('gid_2')]
+    
+    # ----------------------------------------------
     result_rows = []
     
-    # Case 1: Data has ADM2 (regardless of ADM1)
+    # Case 1: Data has ADM2_GAUL (regardless of ADM1_GAUL)
     if adm2_entries:
         for entry in adm2_entries:
             new_row = {
@@ -57,17 +72,45 @@ def reorganize_admin_data_gaul(admin_list):
             }
             result_rows.append(new_row)
             
-    # Case 2: Data has no ADM2 but has ADM1
+    # Case 2: Data has no ADM2_GAUL but has ADM2_GADM (regardless of ADM1)
+    elif adm2_entries_gadm:
+        for entry in adm2_entries_gadm:
+            new_row = {
+                'adm2_name_gadm': entry.get('name_2'),
+                'adm2_code_gadm': entry.get('gid_2'),
+                # Integrate ADM1 list into each ADM2 entry
+                'adm1_name_list_gadm': adm1_names_gadm if adm1_names_gadm else [],
+                'adm1_code_list_gadm': adm1_codes_gadm if adm1_codes_gadm else [],
+                'adm1_name_list': adm1_names if adm1_names else [],
+                'adm1_code_list': adm1_codes if adm1_codes else []
+            }
+            result_rows.append(new_row)
+    
+    # Case 3: Data has no ADM2_GAUL but has ADM1_GAUL (regardless of ADM1_GADM)
     elif adm1_names or adm1_codes:
         new_row = {
             'adm2_name': None,
             'adm2_code': None,
             'adm1_name_list': adm1_names,
-            'adm1_code_list': adm1_codes
+            'adm1_code_list': adm1_codes,
+            'adm1_name_list_gadm': adm1_names_gadm if adm1_names_gadm else [],
+            'adm1_code_list_gadm': adm1_codes_gadm if adm1_codes_gadm else []
+        }
+        result_rows.append(new_row)
+
+    # Case 4: Data has neither ADM2_GAUL nor ADM1_GAUL but has ADM1_GADM
+    elif adm1_names_gadm or adm1_codes_gadm:
+        new_row = {
+            'adm2_name': None,
+            'adm2_code': None,
+            'adm1_name_list': None,
+            'adm1_code_list': None,
+            'adm1_name_list_gadm': adm1_names_gadm,
+            'adm1_code_list_gadm': adm1_codes_gadm
         }
         result_rows.append(new_row)
         
-    # Case 3: Data has neither ADM2 nor ADM1
+    # Case 5: Data has neither ADM2 nor ADM1
     else:
         result_rows.append({})
         
@@ -83,7 +126,7 @@ def preprocess_data(filepath):
     emdat_data = pd.read_csv(filepath)
     
     # ----------------------------------------------
-    emdat_derived = emdat_data[['DisNo.', 'ISO', 'Country', 'Location', 'Latitude', 'Longitude', 'Start Year', 'Start Month', 'Start Day', 'End Year', 'End Month', 'End Day', 'Admin Units']].copy()
+    emdat_derived = emdat_data[['DisNo.', 'ISO', 'Country', 'Location', 'Latitude', 'Longitude', 'Start Year', 'Start Month', 'Start Day', 'End Year', 'End Month', 'End Day', 'Admin Units', 'GADM Admin Units']].copy()
 
     # ----------------------------------------------
     emdat_derived['start_date'] = pd.to_datetime(emdat_derived[['Start Year', 'Start Month', 'Start Day']].rename(
@@ -99,7 +142,8 @@ def preprocess_data(filepath):
     # ----------------------------------------------
     print("Parsing and Restructuring Admin Units...")
     emdat_derived['admin_list_raw'] = emdat_derived['Admin Units'].apply(parse_admin_units_safe)
-    emdat_derived['admin_list_structured'] = emdat_derived['admin_list_raw'].apply(reorganize_admin_data_gaul)
+    emdat_derived['admin_list_raw_gadm'] = emdat_derived['GADM Admin Units'].apply(parse_admin_units_safe)
+    emdat_derived['admin_list_structured'] = emdat_derived.apply(lambda row: reorganize_admin_data(row['admin_list_raw'], row['admin_list_raw_gadm']), axis=1)
     # Explode the admin_list to have one row per admin unit
     emdat_exploded = emdat_derived.explode('admin_list_structured').reset_index(drop=True)
     # Expand the dictionaries in admin_list into separate columns (for easier processing)
@@ -112,13 +156,17 @@ def preprocess_data(filepath):
 # ----------------------------------------------
 # Field: 'bbox'
 # Define a function to get bounding box from GEE
-def get_bbox_from_gee(row, gaul_dataset):
+def get_bbox_from_gee(row, gaul_dataset, gadm_dataset):
     
     country = row['Country']
 
     adm2_code = row['adm2_code'] if pd.notna(row['adm2_code']) else None
     adm2_name = row['adm2_name'] if pd.notna(row['adm2_name']) else None
     adm1_list = row['adm1_name_list'] if isinstance(row.get('adm1_name_list'), list) and len(row['adm1_name_list']) > 0 else None
+
+    adm2_code_gadm = row['adm2_code_gadm'] if pd.notna(row.get('adm2_code_gadm')) else None
+    adm2_name_gadm = row['adm2_name_gadm'] if pd.notna(row.get('adm2_name_gadm')) else None
+    adm1_list_gadm = row['adm1_name_list_gadm'] if isinstance(row.get('adm1_name_list_gadm'), list) and len(row.get('adm1_name_list_gadm')) > 0 else None
     
     try:
         lon = float(row['Longitude'])
@@ -167,7 +215,42 @@ def get_bbox_from_gee(row, gaul_dataset):
             target_feature = filtered.first()
             match_method = 'ADM0&2_NAME'
 
-    # --- 4. If no match by name, and coordinates are available, use coordinates lookup ---
+    # --- 4. If no match above, use adm2_code_gadm ---
+    if target_feature is None and adm2_code_gadm is not None:
+        try:
+            code_int = int(adm2_code_gadm)
+            matches = gadm_dataset[gadm_dataset['GID_2'] == code_int]
+            
+            if not matches.empty:
+                target_feature = matches.iloc[0]
+                match_method = 'ADM2_CODE_GADM'
+        except:
+            pass
+    
+    # --- 5. If no match above, use adm2_name_gadm + adm1_name_gadm + Country ---
+    if target_feature is None and adm2_name_gadm is not None and adm1_list_gadm is not None:
+        matches = gadm_dataset[
+            (gadm_dataset['COUNTRY'] == country) & 
+            (gadm_dataset['NAME_2'] == adm2_name_gadm) & 
+            (gadm_dataset['NAME_1'].isin(adm1_list_gadm))
+        ]
+        
+        if not matches.empty:
+            target_feature = matches.iloc[0]
+            match_method = 'ADM0&1&2_NAME_GADM'
+
+    # --- 6. If no match above, use adm2_name_gadm + Country ---
+    if target_feature is None and adm2_name_gadm is not None:
+        matches = gadm_dataset[
+            (gadm_dataset['COUNTRY'] == country) & 
+            (gadm_dataset['NAME_2'] == adm2_name_gadm)
+        ]
+        
+        if not matches.empty:
+            target_feature = matches.iloc[0]
+            match_method = 'ADM0&2_NAME_GADM'
+    
+    # --- 7. If no match by name, and coordinates are available, use coordinates lookup ---
     if target_feature is None and has_coords:
         point = ee.Geometry.Point([lon, lat])
         filtered = gaul_dataset.filterBounds(point)
@@ -176,7 +259,7 @@ def get_bbox_from_gee(row, gaul_dataset):
             target_feature = filtered.first()
             match_method = 'Coordinates_Lookup'
     
-    # --- 5. If no match found, return error message ---
+    # --- 8. If no match found, return error message ---
     if target_feature is None:
         # print(f"event_id: {event_id} cannot be located")
         return {
@@ -204,18 +287,26 @@ def get_bbox_from_gee(row, gaul_dataset):
 
 # ----------------------------------------------
 # Main function
-def main():    
+def main():
 
-    # Filepath settings
-    # Google Earth Engine project name setting
+    # Filepath settings & Google Earth Engine project name setting
     input_filepath = '/home/chunen/nas/HAZAMA_data/public_emdat_custom_request_2026-01-28.csv'
     output_filepath = '/home/chunen/HAZAMA/HAZAMA/outputs/data_ingestion.csv'
     MY_GEE_PROJECT = 'oceanic-hash-467505-r2'
+    # GADM GeoPackage filepath setting
+    gadm_filepath = '/home/chunen/nas/HAZAMA_data/gadm_410-levels-ADM2.gpkg'
     
-    # A. Initialize GEE
+    # A-1. Initialize GEE and read GAUL dataset
     initialize_gee(MY_GEE_PROJECT)
-    # Read GAUL dataset
     gaul = ee.FeatureCollection('FAO/GAUL/2015/level2')
+
+    # A-2. Load GADM dataset
+    print("Loading GADM GeoPackage...")
+    gadm = gpd.read_file(gadm_filepath)
+    # Reproject to EPSG:4326 if the CRS is different
+    if gadm.crs != 'EPSG:4326':
+        print("Reprojecting GeoDataFrame to EPSG:4326...")
+        gadm = gadm.to_crs('EPSG:4326')
 
     # B. Read and process data
     try:
@@ -233,19 +324,19 @@ def main():
 
     # D. Execute query (use lambda to pass gaul into the function)
     df_to_process['bbox_result'] = df_to_process.progress_apply(
-        lambda row: get_bbox_from_gee(row, gaul), axis=1
+        lambda row: get_bbox_from_gee(row, gaul, gadm), axis=1
     )
 
     # E. Organize results
     bbox_df = pd.json_normalize(df_to_process['bbox_result'])
     final_df = pd.concat([df_to_process.reset_index(drop=True), bbox_df], axis=1)
 
-    # Check which locations could not be found
+    # Check how many events could not be located
     missing_count = len(final_df[final_df['match_method'] == 'cannot_be_located'])
     print(f"There are **{missing_count}** records that could not be located")
 
     # F. Output file
-    output = final_df[['event_id', 'start_date', 'end_date', 'bbox']]
+    output = final_df[['event_id', 'start_date', 'end_date', 'bbox', 'match_method']]
     output.to_csv(output_filepath, index=False)
     print(f"The Data for ingestion is saved to {output_filepath}")
 
